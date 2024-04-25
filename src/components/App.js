4b4008a3af8b9e0784bf4e605ca29788774979f5
Narreddy Saini <narreddysaini1506@gmail.com>
2024-04-25 09:19:57 +0530

    creating cards using prop-types

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Header from './Header';
+import Hero from './Hero';
+import Articles from './Articles';
+import Footer from './Footer';
+
+function App(){
+    return(
+        <div>
+            <Header />
+            <Hero />
+            <Articles />
+            <Footer />
+        </div>
+    );
+}
+export default App;
